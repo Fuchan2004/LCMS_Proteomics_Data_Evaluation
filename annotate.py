2a55e21563fcbd33c8708352d111f3771438829a
Fadime Stemmer <fadimestemmer@Fadimes-MacBook-Pro.local>
2024-08-14 09:34:02 -0400

    Finished file annotate.py to annotate files in case annotations are missing.

--- annotate.py
+++ annotate.py
@@ -1,29 +1,62 @@
 #!/usr/bin/env python3
 
 '''
-This script is used to replace Accession Numbers with Annotations in the column `Identified Proteins (XXX)` / `Annotations`. 
+This script is used to replace Accession Numbers with Annotations in the column `Annotations`. The script can only be used on the formatted proteome files that have the extension *_formatted.txt.
 
-USAGE: python annotate.py <foldername>
+USAGE: python annotate.py </path/annotations_file.txt> </path/input_foldername/>
 '''
 
 import pandas as pd
 import glob
+import os
+import sys
 
-def annotate_DSS3(input='', output_filename=''):
-  with open(input, 'r') as input_file:\n
-      with open(output_filename, 'w') as output_file:\n
-      # Write the header line to the output file
-        header = input_file.readline().strip() + '\\tAnnotation_Uniprot\\n'
+# Reading in the SPO numbers and associated annotations and saving them in the dictionary SPO_dict
+input_file = sys.argv[1]
+SPO_dict = {}
+
+with open(input_file, 'r') as file:
+    for line in file:
+        spo, annotation = line.strip().split('\t', 1)  # Split the line at the first tab character
+        SPO_dict[spo] = annotation
+
+# Function to annotate the files (Replacing values in column 2) if annotations are not provided
+def annotate(folder=''):
+    for filepath in glob.glob(os.path.join(folder, '*_formatted.txt')):
+        filename = os.path.basename(filepath)  # Get the filename
+        print(f"Processing file: {filename}")
+    
+    output_file_path = filepath.replace('_formatted.txt', '_annotated.txt')
+    
+    with open(filepath, 'r') as input_file, open(output_file_path, 'w') as output_file:
+        # Write the header line to the output file
+        header = input_file.readline().strip() + '\n'
         output_file.write(header)
-      # Process each line in the input file
+        
+        # Process each line in the input file
         for line in input_file:
-      # Split the line into columns
-          columns = line.strip().split('\\t')
-          spo_id = columns[3]  # Extract the SPO ID from column 4
-      # Look up the annotation in the dictionary\n",
-          annotation = SPO_dict.get(spo_id, 'Unknown')\n",
-      # Append the annotation to the end of the line and write to the output file\n",
-          output_line = '\\t'.join(columns) + '\\t' + annotation + '\\n'
-          output_file.write(output_line)
+            # Split the line into columns
+            columns = line.strip().split('\\t')
+            spo_id = columns[2]  # Extract the SPO ID from column 3
+            # Look up the annotation in the dictionary
+            annotation = SPO_dict.get(spo_id, 'Unknown')
+            columns[1] = annotation
+            # Join the columns back into a line and write to the output file
+            output_line = '\t'.join(columns) + '\n'
+            output_file.write(output_line)
+                
+        with open(output_file_path, 'w') as file:
+            file.write(header)
+            file.writelines(output_line)
+    
+    print(f"Annotated lines written to: {output_file_path}")
 
 
+if __name__ == "__main__": 
+    
+    if len(sys.argv) !=3: # If there is more than 2 argument to call this script, it will provide guidance on how to use it.
+        print("Usage: python annotate.py </path/annotations_file.txt> </path/input_foldername/>")
+        sys.exit(1)
+    
+    folder = sys.argv[2]
+    annotate(folder)
