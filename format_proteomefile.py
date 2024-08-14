b83ff4cbf2253d1beadcebe43c4d222a8bdf2e62
Fadime Stemmer <139340364+Fuchan2004@users.noreply.github.com>
2024-08-13 21:50:36 -0400

    Added print statements for debugging format_proteomefile.py

--- format_proteomefile.py
+++ format_proteomefile.py
@@ -18,8 +18,9 @@ def format_proteomefiles(folder=''):
     selected_columns = [0, 2, 3, 8, 9, 10, 11]
     
     for files in glob.glob(folder + '*.txt'):
+        filename = file.split('/')[-1] #define the filename 
         # Read the original file
-        with open(files, 'r') as file:
+        with open(folder+filename, 'r') as file:
             lines = files.readlines()
         
         # First, rename columns such that they are compatible with the scripts.
@@ -28,6 +29,7 @@ def format_proteomefiles(folder=''):
         for line in lines:
             # Split the line into columns based on tab delimiter
             columns = line.strip().split('\t')
+            print(columns)
             
             # Rename columns
             if len(columns) == 12:
@@ -39,6 +41,7 @@ def format_proteomefiles(folder=''):
 
             # Filter selected columns
             filtered_columns = [columns[i] for i in selected_columns]
+            print(filtered_columns)
             
             # Join the filtered columns back into a line
             processed_line = '\t'.join(filtered_columns) + '\n'
@@ -47,13 +50,16 @@ def format_proteomefiles(folder=''):
             # Join the columns back into a line
             renamed_line = '\t'.join(columns) + '\n'
             renamed_lines.append(renamed_line)
+            print(renamed_lines)
 
         # Sort by accession number
         sorted_lines = sorted(renamed_lines, key=lambda x: x.split('\t')[3])  
+        print(sorted_lines)
         
         
         # Write the sorted and filtered lines back to a new file
         output_file_path = file_path.replace('.txt', '_formatted.txt')
+        print(output_file_path)
         
         with open(output_file_path, 'w') as file:
             file.writelines(sorted_lines)
@@ -64,5 +70,15 @@ def format_proteomefiles(folder=''):
 if __name__ == "__main__": 
     
     if len(sys.argv) !=2: # If there is more than 1 argument to call this script, it will provide guidance on how to use it.
-        print("Usage: python format_proteomefile.py <foldername>")
+        print("Usage: python format_proteomefile.py <input_foldername>")
         sys.exit(1)
+'''
+    for frame, translation in translation_dict.items(): 
+            output_list.append(frame + '\t' + translation) # fill list with frame tab delimited with the respective translation from the translation_dict (= output of six-frame-tranlation.py)
+        full_output = '\n'.join(output_list) # define full_output, so we separate each frame + translation into new lines. 
+
+        outfilename = output_file
+
+        with open(outfilename, 'w') as f: # write the frame + translation into new file with user specified filename.
+            f.write(full_output)
+'''
