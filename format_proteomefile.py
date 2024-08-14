6e352408980b2b5c815491bb681d9efe5b203695
Fadime Stemmer <139340364+Fuchan2004@users.noreply.github.com>
2024-08-13 22:12:37 -0400

    Change one column in selected column from 2 to 1 format_proteomefile.py

--- format_proteomefile.py
+++ format_proteomefile.py
@@ -15,7 +15,7 @@ import sys
 
 def format_proteomefiles(folder=''):
     
-    selected_columns = [1, 3, 4, 9, 10, 11, 12]
+    selected_columns = [0, 3, 4, 9, 10, 11, 12]
 
     for filepath in glob.glob(os.path.join(folder, '*.txt')):
         filename = os.path.basename(filepath)  # Get the filename
